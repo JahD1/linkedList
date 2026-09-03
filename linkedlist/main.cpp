4a8b29e2bf7ef3dfaa241f703d156a8c0bda198d
Nadjah Duke <nadjahduke@gmail.com>
2026-09-03 09:48:41 -0400

    Initial Commit

---
+++ linkedlist/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  linkedlist
+//
+//  Created by Nadjah Duke on 9/3/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
